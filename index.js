d43e4575546f855b16038727eb6cc633da241556
eliav-cpu <eliav@puzzle-group.co.il>
2026-06-02 09:16:04 +0300

    Stabilize legacy formula API for EXCELDOR baseline

--- index.js
+++ index.js
@@ -1,477 +1,168 @@
-  ////////////////////////////////
- //// Gross Potential Income ////
-////////////////////////////////
+const valuesFromArgs = (argsLike, names) => {
+  const args = Array.from(argsLike);
+  if (args.length === 1 && args[0] && typeof args[0] === 'object' && !Array.isArray(args[0])) {
+    return names.map((name) => Number(args[0][name] || 0));
+  }
+  return names.map((_, index) => Number(args[index] || 0));
+};
+
+const finite = (value, fallback = 0) => (Number.isFinite(Number(value)) ? Number(value) : fallback);
+const fixed = (value, digits = 2) => Number(finite(value).toFixed(digits));
 
 const annualGrossPotentialIncome = (expectedMonthlyRentIncome) => {
-  return (12 * expectedMonthlyRentIncome)
-}
-
-//EXAMPLE FOR ANUAL GROSS POTENTIAL INCOME
-// annualGrossPotentialIncome(34000)
-
-
-
-  ////////////////////////////////
- //// Net Operating Income ////
-////////////////////////////////
-
-///////**** Methodology ****//////
-// Gross Potential Income
-// subtracted by Vacancy and
-// Credit Losses
-const netOperatingIncome = function({
-  monthlyIncome,
-  vacancies,
-  nonPayments,
-  taxes,
-  mortgageInterest,
-  marketing,
-  advertising,
-  management,
-  legal,
-  accounting,
-  utilities,
-  repairs,
-  maintenance,
-  acquisition,
-  sale_costs
-}){
-  let args = Object.values(arguments);
-  let gpi = annualGrossPotentialIncome(args[0]);
-  let totalExpenses = args.slice(1, args.length -1).reduce((a, b) => a + b, 0);
-  let result = gpi - totalExpenses;
-  return result;
-}
-
-//EXAMPLE
-// netOperatingIncome(
-//   34000,
-//   850,
-//   850,
-//   100,
-//   2000,
-//   200,
-//   100,
-//   20000,
-//   2000,
-//   30000,
-//   100,
-//   200,
-//   3000,
-//   100000,
-//   500000);
-
-
-
-
-  //////////////////////////////////
- ///// Gross Rental Multiplier ////
-//////////////////////////////////
-
-///////**** Methodology ****//////
-// divide market value by
-// annual gross potential income
-
-const grossRentalMultiplier = function({
-  marketValue,
-  agi
-}){
-  let args = Object.values(arguments);
-  return ((args[0]/args[1]).toFixed(3));
+  return finite(expectedMonthlyRentIncome) * 12;
 };
 
-//EXAMPLE OF GRM CALCULATOR
-// grossRentalMultiplier(543000, annualGrossPotentialIncome(340000));
+const netOperatingIncome = function () {
+  const [
+    monthlyIncome,
+    vacancies,
+    nonPayments,
+    taxes,
+    mortgageInterest,
+    marketing,
+    advertising,
+    management,
+    legal,
+    accounting,
+    utilities,
+    repairs,
+    maintenance,
+    acquisition,
+    saleCosts,
+  ] = valuesFromArgs(arguments, [
+    'monthlyIncome',
+    'vacancies',
+    'nonPayments',
+    'taxes',
+    'mortgageInterest',
+    'marketing',
+    'advertising',
+    'management',
+    'legal',
+    'accounting',
+    'utilities',
+    'repairs',
+    'maintenance',
+    'acquisition',
+    'sale_costs',
+  ]);
 
-
-
-
-   //////////////////////////////////
-  //// Estimated Property Value ////
- ////          (GRM)           ////
-//////////////////////////////////
-
-///////**** Methodology ****//////
-// Gross Rent Multiplier
-// multiplied by Annual Income
-// Note: Not the Gross Annual
-// Income
-
-const estimatedPropertyValueByGRM = function({
-  grm,
-  annualIncome
-}){
-  let args = Object.values(arguments);
-  return args[0] * args[1];
-}
-
-
-//EXAMPLE OF ESTIMATED PROPERTY VALUE BY GRM CALCULATOR
-// estimatedPropertyValueByGRM(grossRentalMultiplier(543000, annualGrossPotentialIncome(34000)), 340000)
-
-
-  ////////////////////////////////
- //// Gross Operating Income ////
-////////////////////////////////
-
-///////**** Methodology ****//////
-// Gross Potential Income
-// subtracted by Vacancies and
-// Non-Payments
-
-
-const grossOperatingIncome = function({
-  monthlyIncome,
-  estimatedLossPercentage
-}){
-  let args = Object.values(arguments);
-  let agi = annualGrossPotentialIncome(args[0]);
-  let estimatedLoss = agi * (args[1] / 100).toFixed(2);
-  return (agi - estimatedLoss);
-}
-
-//EXAMPLE OF GROSS OPERATING INCOME
-// grossOperatingIncome(34000, 5);
-
-
-
-  ////////////////////////////////
- /////  Capitalization Rate /////
-////////////////////////////////
-
-///////**** Methodology ****//////
-// Gross Potential Income
-// subtracted by Vacancy and
-// Credit Losses (except mortage)
-// all divided by sale price
-
-const capitalizationRate = function({
-  monthlyIncome,
-  vacancies,
-  nonPayments,
-  taxes,
-  marketing,
-  advertising,
-  management,
-  legal,
-  accouting,
-  utilities,
-  repairs,
-  maintenance,
-  acquisition,
-  sale_costs,
-  sale_earned
-}){
-  let args = Object.values(arguments);
-  let gpi = annualGrossPotentialIncome(args[0]);
-  let totalExpenses = args.slice(1, args.length -2).reduce((a, b) => a + b, 0);
-  let noi = (gpi - totalExpenses);
-  let result = (noi / args[args.length -1]).toFixed(3);
-  return result;
-}
-
-
-//EXAMPLE FOR CAP RATE
-// capitalizationRate(
-//   34000,
-//   850,
-//   850,
-//   100,
-//   200,
-//   100,
-//   20000,
-//   2000,
-//   30000,
-//   100,
-//   200,
-//   3000,
-//   100000,
-//   500000);
-
-
-
-
-
-   //////////////////////////////////
-  //// Estimated Property Value ////
- /////       (Cap Rate)       /////
-//////////////////////////////////
-
-///////**** Methodology ****//////
-// Gross Rent Multiplier
-// multiplied by Annual Income
-// Note: Not the Gross Annual
-// Income
-
-const estimatedPropertyValueByCapRate = function({
-  noi,
-  capRate
-}){
-  let args = Object.values(arguments);
-  return (args[0]/args[1]).toFixed(2);
-}
-
-
-//EXAMPLE OF ESTIMATED PROPERTY
-//VALUE BY CAP RATE
-
-// estimatedPropertyValueByCapRate(
-  // netOperatingIncome(
-  // 34000,
-  // 850,
-  // 850,
-  // 100,
-  // 2000,
-  // 200,
-  // 100,
-  // 20000,
-  // 2000,
-  // 30000,
-  // 100,
-  // 200,
-  // 3000,
-  // 100000,
-  // 30000),
-//   capitalizationRate(
-//   34000,
-//   850,
-//   850,
-//   100,
-//   200,
-//   100,
-//   20000,
-//   2000,
-//   30000,
-//   100,
-//   200,
-//   3000,
-//   100000,
-//   30000,
-//   500000)
-// )
-
-
-
-
-  //////////////////////////////////
- //// Cash Flow Before Taxes   ////
-//////////////////////////////////
-
-///////**** Methodology ****//////
-// Net Operating Income subtracted
-// by Debt Service (mortgage interest
-// and principle) subtracted by
-// all capital expenditures
-// subtracted by all loans
-// taken for capital expenditures
-// plus all interest earned/generated
-// from the subject property
-
-
-
-const cashFlowBeforeTaxes = function({
-  noi,
-  interestRate,
-  loanPrinciple,
-  capitalExpenditures,
-  capitalExpenditureLoans,
-  earnedInterest
-}){
-  let args = Object.values(arguments);
-  return (args[0] - (((args[1] / 100) * args[2]).toFixed(2) + args[2] + args[3])).toFixed(2) - args[4] + args[5];
-}
-
-
-//EXAMPLE FOR CASHFLOW BEFORE TAXES
-// cashFlowBeforeTaxes(
-//   netOperatingIncome(
-//     34000,
-//     850,
-//     850,
-//     100,
-//     2000,
-//     200,
-//     100,
-//     20000,
-//     2000,
-//     30000,
-//     100,
-//     200,
-//     3000,
-//     100000,
-//     30000
-//   ),
-//   10,
-//   540000,
-//   20000,
-//   3000,
-//   15000
-// );
-
-
-
-
-
-
-  //////////////////////////////////
- //// Cash Flow After Taxes    ////
-//////////////////////////////////
-
-///////**** Methodology ****//////
-// Cash Flow Before Taxes subtracted
-// by income tax (State and Federal)
-
-
-const cashFlowAfterTaxes = function({
-  cfbt,
-  stateIncomeTax,
-  federalIncomeTax
-}){
-  let args = Object.values(arguments);
-  return args[0] - (args[1] + args[2]).toFixed(2);
+  const grossPotentialIncome = annualGrossPotentialIncome(monthlyIncome);
+  const totalExpenses = vacancies + nonPayments + taxes + mortgageInterest + marketing + advertising + management + legal + accounting + utilities + repairs + maintenance + acquisition + saleCosts;
+  return fixed(grossPotentialIncome - totalExpenses);
 };
 
-
-// cashFlowAfterTaxes(
-//   cashFlowBeforeTaxes(
-//     netOperatingIncome(
-//       34000,
-//       850,
-//       850,
-//       100,
-//       2000,
-//       200,
-//       100,
-//       20000,
-//       2000,
-//       30000,
-//       100,
-//       200,
-//       3000,
-//       100000,
-//       30000
-//     ),
-//     10,
-//     540000,
-//     20000,
-//     3000,
-//     15000
-//   ),
-//   -45000,
-//   12000
-// );
-
-
-
-
-  ////////////////////////////////
- /////   Break Even Ratio   /////
-////////////////////////////////
-
-
-///////**** Methodology ****//////
-// Debt Service plus
-// Annual Operating Expenses
-// divided by Gross Operating
-// Income
-
-const breakEvenRatio = function({
-  interestRate,
-  loanPrinciple,
-  marketing,
-  advertising,
-  management,
-  legal,
-  accounting,
-  utilities,
-  repairs,
-  maintenance,
-  goi
-}){
-  let args = Object.values(arguments);
-  let debtService = ((args[0] / 100) * args[1] + args[1]);
-  let operatingExpenses = args.slice(2, args.length - 2).reduce((acc, sum) => acc + sum, 0)
-  let expenses = ((debtService) + operatingExpenses);
-  let result = (expenses / args[args.length - 1]).toFixed(2);
-  return result;
+const grossRentalMultiplier = function () {
+  const [marketValue, agi] = valuesFromArgs(arguments, ['marketValue', 'agi']);
+  if (agi === 0) return null;
+  return fixed(marketValue / agi, 3);
 };
 
-
-//EXAMPLE OF BREAK EVEN RATIO
-// breakEvenRatio(
-//   10,
-//   20000,
-//   1000,
-//   500,
-//   15000,
-//   3500,
-//   28000,
-//   3200,
-//   8000,
-//   1500,
-//   grossOperatingIncome(34000, 5)
-// );
-
-
-
-
-  ////////////////////////////////
- /////   Return On Equity   /////
-////////////////////////////////
-
-
-///////**** Methodology ****//////
-// Cash Flow After Taxes
-// divided by Principle
-// Invested on subject property
-
-const returnOnEquity = function({
-  cfat,
-  principleInvested
-}){
-  let args = Object.values(arguments);
-  let result = (args[0] / args[1]).toFixed(2);
-  return result;
+const estimatedPropertyValueByGRM = function () {
+  const [grm, annualIncome] = valuesFromArgs(arguments, ['grm', 'annualIncome']);
+  return fixed(grm * annualIncome);
 };
 
+const grossOperatingIncome = function () {
+  const [monthlyIncome, estimatedLossPercentage] = valuesFromArgs(arguments, ['monthlyIncome', 'estimatedLossPercentage']);
+  const annualIncome = annualGrossPotentialIncome(monthlyIncome);
+  const estimatedLoss = annualIncome * (estimatedLossPercentage / 100);
+  return fixed(annualIncome - estimatedLoss);
+};
 
+const capitalizationRate = function () {
+  const [
+    monthlyIncome,
+    vacancies,
+    nonPayments,
+    taxes,
+    marketing,
+    advertising,
+    management,
+    legal,
+    accounting,
+    utilities,
+    repairs,
+    maintenance,
+    acquisition,
+    saleCosts,
+    saleEarned,
+  ] = valuesFromArgs(arguments, [
+    'monthlyIncome',
+    'vacancies',
+    'nonPayments',
+    'taxes',
+    'marketing',
+    'advertising',
+    'management',
+    'legal',
+    'accounting',
+    'utilities',
+    'repairs',
+    'maintenance',
+    'acquisition',
+    'sale_costs',
+    'sale_earned',
+  ]);
 
-//EXAMPLE FOR RETURN ON EQUITY
+  if (saleEarned === 0) return null;
+  const grossPotentialIncome = annualGrossPotentialIncome(monthlyIncome);
+  const totalExpenses = vacancies + nonPayments + taxes + marketing + advertising + management + legal + accounting + utilities + repairs + maintenance + acquisition + saleCosts;
+  return fixed((grossPotentialIncome - totalExpenses) / saleEarned, 3);
+};
 
-// returnOnEquity(
-//   cashFlowAfterTaxes(
-//     cashFlowBeforeTaxes(
-//       netOperatingIncome(
-//         34000,
-//         850,
-//         850,
-//         100,
-//         2000,
-//         200,
-//         100,
-//         20000,
-//         2000,
-//         30000,
-//         100,
-//         200,
-//         3000,
-//         100000,
-//         30000
-//       ),
-//       10,
-//       540000,
-//       20000,
-//       3000,
-//       15000
-//     ),
-//     -45000,
-//     12000
-//   ),
-//   6500000
-// );
+const estimatedPropertyValueByCapRate = function () {
+  const [noi, capRate] = valuesFromArgs(arguments, ['noi', 'capRate']);
+  if (capRate === 0) return null;
+  return fixed(noi / capRate);
+};
 
+const cashFlowBeforeTaxes = function () {
+  const [noi, interestRate, loanPrinciple, capitalExpenditures, capitalExpenditureLoans, earnedInterest] = valuesFromArgs(arguments, [
+    'noi',
+    'interestRate',
+    'loanPrinciple',
+    'capitalExpenditures',
+    'capitalExpenditureLoans',
+    'earnedInterest',
+  ]);
 
+  const debtService = (interestRate / 100) * loanPrinciple + loanPrinciple;
+  return fixed(noi - debtService - capitalExpenditures - capitalExpenditureLoans + earnedInterest);
+};
 
+const cashFlowAfterTaxes = function () {
+  const [cfbt, stateIncomeTax, federalIncomeTax] = valuesFromArgs(arguments, ['cfbt', 'stateIncomeTax', 'federalIncomeTax']);
+  return fixed(cfbt - stateIncomeTax - federalIncomeTax);
+};
+
+const breakEvenRatio = function () {
+  const [interestRate, loanPrinciple, marketing, advertising, management, legal, accounting, utilities, repairs, maintenance, goi] = valuesFromArgs(arguments, [
+    'interestRate',
+    'loanPrinciple',
+    'marketing',
+    'advertising',
+    'management',
+    'legal',
+    'accounting',
+    'utilities',
+    'repairs',
+    'maintenance',
+    'goi',
+  ]);
+
+  if (goi === 0) return null;
+  const debtService = (interestRate / 100) * loanPrinciple + loanPrinciple;
+  const operatingExpenses = marketing + advertising + management + legal + accounting + utilities + repairs + maintenance;
+  return fixed((debtService + operatingExpenses) / goi);
+};
+
+const returnOnEquity = function () {
+  const [cfat, principleInvested] = valuesFromArgs(arguments, ['cfat', 'principleInvested']);
+  if (principleInvested === 0) return null;
+  return fixed(cfat / principleInvested);
+};
 
 module.exports = {
   annualGrossPotentialIncome,
@@ -484,5 +175,5 @@ module.exports = {
   cashFlowBeforeTaxes,
   cashFlowAfterTaxes,
   breakEvenRatio,
-  returnOnEquity
-}
+  returnOnEquity,
+};
